6958795a3bc634478ea63c2f0e1cc2993b77f4ab
Ikhsan Nur <ikhsan15nur@gmail.com>
2021-09-13 13:17:04 +0700

    create header

--- js/main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", () =>{
-  // preloader
-  document.querySelector(".preloader").classList.add("fade-out");
-  setTimeout(() =>{
-    document.querySelector(".preloader").style.display="none";
-  }, 600)
-})
