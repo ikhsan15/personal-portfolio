b6870b883f54d0b17f27bafa3b31c422ff87cb03
Ikhsan Nur <ikhsan15nur@gmail.com>
2021-09-13 14:57:48 +0700

    create section institutional, main skills

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+(() =>{
+  $("#pie-1").percircle({ progressBarColor: '#88ff08' });
+  $("#pie-2").percircle({ progressBarColor: '#88ff08' });
+  $("#pie-3").percircle({ progressBarColor: '#88ff08' });
+  $("#pie-4").percircle({ progressBarColor: '#88ff08' });
+})();
